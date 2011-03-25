cef2d5028ce9e6f20c818c246219eee62669757a
Ming <minger@gmail.com>
2011-03-24 21:45:19 -0400

    fix namespaces.rb to be consistent with module name change from Rails3::Assist to RailsAssist

--- lib/rails_assist/namespaces.rb
+++ lib/rails_assist/namespaces.rb
@@ -1,11 +1,9 @@
 require 'sugar-high/module'
 
-module Rails3
-  module Assist
-    modules :app, :directory, :files, :file
-    modules :artifact do
-      nested_modules :directory, :files, :path
-    end
+module RailsAssist
+  modules :app, :directory, :files, :file
+  modules :artifact do
+    nested_modules :directory, :files, :path
   end
 end
     
