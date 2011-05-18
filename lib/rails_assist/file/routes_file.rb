12a2357a04f17738776226ed580299158d1242ed
Kristian Mandrup <kmandrup@gmail.com>
2011-05-18 13:41:38 +0200

    updated route insertion API and docu

--- lib/rails_assist/file/routes_file.rb
+++ lib/rails_assist/file/routes_file.rb
@@ -1,13 +1,18 @@
 module RailsAssist::File
-  module Routes
+  module Routes 
+    # insert as first route statement
     def insert_into_routes route_stmt=nil, &block
       statement = block ? yield : route_stmt
       routes_file.insert statement, :after => 'routes.draw do'
     end
+    alias_method :insert_as_first_route_statement, :insert_into_routes
 
+    # insert as last route statement
     def insert_last_in_routes route_stmt=nil, &block
       statement = block ? yield : route_stmt
       routes_file.insert statement, :before_last => 'end'
     end
+    
+    alias_method :insert_as_last_route_statement, :insert_last_in_routes
   end
 end
